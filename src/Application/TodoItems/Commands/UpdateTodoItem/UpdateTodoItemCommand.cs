239fc25621466919affcbde831bd9f431c4074d8
Bufffer <serdarogluibrahim@gmail.com>
2025-04-09 16:52:49 +0300

    Add BackgroundColor property to TodoItem entity

--- src/Application/TodoItems/Commands/UpdateTodoItem/UpdateTodoItemCommand.cs
+++ src/Application/TodoItems/Commands/UpdateTodoItem/UpdateTodoItemCommand.cs
@@ -12,6 +12,8 @@ public record UpdateTodoItemCommand : IRequest
     public string? Title { get; init; }
 
     public bool Done { get; init; }
+
+    public string? BackgroundColor { get; init; }
 }
 
 public class UpdateTodoItemCommandHandler : IRequestHandler<UpdateTodoItemCommand>
@@ -35,6 +37,8 @@ public class UpdateTodoItemCommandHandler : IRequestHandler<UpdateTodoItemComman
 
         entity.Title = request.Title;
         entity.Done = request.Done;
+        entity.BackgroundColor = request.BackgroundColor ?? entity.BackgroundColor;
+
 
         await _context.SaveChangesAsync(cancellationToken);
 
